20ded0f6a3d80db22f88857e9bf6ab55b4a91dfd
mihirbpi <mihir.borkar@gmail.com>
2019-08-15 15:37:02 -0700

    -Cleaned up code

--- newton_second_law_simulator.js
+++ newton_second_law_simulator.js
@@ -1,74 +1,134 @@
-function getForce(x, y) {
-  /* Function for force on object at point(x,y) */
-
-  //Projectile in Earth's gravitational field
-  if (is_Projectile) {
-    if (y > 0) {
-      force = new Vector(0, 0);
-      v.info_array[0] = 0, v.info_array[1] = 0;
-      return force;
-    }
-
-    force = new Vector(0, mass * gravity);
-    return force;
-  }
-   //Spring in x direction
-  else if(is_Spring){
-     force = new Vector(-k * x, 0);
-     return force;
-  }
-}
-
 function setup() {
   /* Setup the animation */
-
-  var is_Spring = false;
-  var is_Projectile = false;
-
-  var buttonProjectile = createButton("<b>Start/Reset Projectile Simulation</b>");
-  buttonProjectile.size(100, 50);
-  buttonProjectile.mousePressed(resetSketchProjectile);
-  var buttonSpring = createButton("<b>Start/Reset Spring Simulation</b>");
-  buttonSpring.size(100, 50);
-  buttonSpring.mousePressed(resetSketchSpring);
-  resetSketchSpring();
+  initializeSimulations();
+  initializeCanvas();
+  initializePosition();
 
 }
 
-function resetSketchProjectile() {
-  is_Projectile = true;
-  is_Spring = false;
-  initialize(mass = 10, gravity = 9.8, delta_t = 0.1, px = 0, py = 0, vx = 50, vy = -50, radius = 20, //Projectile in Earth's gravitational field
-    mu = 0, k = 0, accel_magnif = 3.5, vel_magnif = 0.5);
-}
-
-function resetSketchSpring() {
-  is_Spring = true;
-  is_Projectile = false;
-  initialize(mass = 10, gravity = 9.8, delta_t = 0.03125, px = -500, py = 0, vx = 0, vy = 0, radius = 20, //Spring in x direction
-    mu = 0, k = 1, accel_magnif = 1, vel_magnif = 1);
-}
-
-
-//createButton("reset");
 function draw() {
   /* The draw loop which is called continuously */
+  updateCanvas();
+  updatePosition();
+}
 
+function initializeCanvas() {
+  createCanvas(window.innerWidth - 10, window.innerHeight - 70);
+  colorMode(RGB);
+  background(255, 255, 255);
   translate(width / 2, height / 2);
-
-  stroke(0, 255, 0);
-  fill(255, 255, 255);
-  rect(-width / 2, -height / 2, width, height);
-
   stroke(101, 67, 33);
   line(-width, 0, width, 0);
-  line(-width, 0, width, 0);
-
   stroke(0, 0, 0);
   fill(0, 0, 0);
-
-  circle(p.info_array[0], p.info_array[1], radius);
   circle(0, 0, 5);
+}
+
+function initializeSimulations() {
+  number_simulations = 2;
+  simulation_names = ["Projectile", "Spring"];
+
+  simulation_functions = [
+    function resetSketchProjectile() {
+      initialize(mass = 10, gravity = 9.8, delta_t = 0.1, px = 0, py = 0, vx = 50, vy = -50, radius = 20, //Projectile in Earth's gravitational field
+        mu = 0, k = 0, accel_magnif = 3.5, vel_magnif = 0.5, simulation = 0);
+    },
+    function resetSketchSpring() {
+      initialize(mass = 10, gravity = 9.8, delta_t = 0.03125, px = -500, py = 0, vx = 0, vy = 0, radius = 20, //Spring in x direction
+        mu = 0, k = 1, accel_magnif = 1, vel_magnif = 1, simulation = 1);
+    }
+  ];
+
+  simulation_force_functions = [
+    function getForceProjectile(x, y) {
+      if (y > 0) {
+        force = new Vector(0, 0);
+        v.info_array[0] = 0, v.info_array[1] = 0;
+        return force;
+      }
+
+      force = new Vector(0, mass * gravity);
+      return force;
+    },
+
+    function getForceSpring(x, y) {
+      force = new Vector(-k * x, 0);
+      return force;
+    }
+  ];
+
+  options = [];
+  for (var i = 0; i < number_simulations; i++) {
+    options.push(false);
+  }
+
+  for (var i = 0; i < number_simulations; i++) {
+    button = createButton("<b>Start/Reset " + simulation_names[i] + " Simulation</b>");
+    button.size(100, 50);
+    button.mousePressed(simulation_functions[i]);
+  }
+
+  simulation_functions[0]();
+}
+
+function initializePosition() {
+  circle(p.info_array[0], p.info_array[1], radius);
+
+  v.setColor(vel_color);
+  v.setMagnif(vel_magnif);
+
+  f = getForce(p.info_array[0], p.info_array[1]);
+
+  a = shrinkVector(f, mass);
+  a.setColor(accel_color);
+  a.setMagnif(accel_magnif);
+  drawVector(p.info_array[0], p.info_array[1], v);
+  drawVector(p.info_array[0], p.info_array[1], a);
+}
+
+function selectSimulation(selection) {
+  selected_simulation = selection
+  for (var i = 0; i < options.length; i++) {
+    if (i == selected_simulation) {
+      options[i] = true;
+    } else {
+      options[i] = false;
+    }
+  }
+}
+
+/* function to initialize Newton's law and create canvas */
+function initialize(m, g, dt, px, py, vx, vy, r, µ, K, a_magnif, v_magnif, selected_sim) {
+
+  mass = m;
+  gravity = g;
+  delta_t = dt;
+  p = new Vector(px, py);
+  v = new Vector(vx, vy);
+  radius = r;
+  mu = µ;
+  k = K;
+  accel_magnif = a_magnif;
+  accel_color = [0, 255, 0];
+  vel_magnif = v_magnif;
+  vel_color = [255, 0, 0];
+
+  selectSimulation(selected_sim);
+}
+
+function updateCanvas() {
+  translate(width / 2, height / 2);
+  fill(255, 255, 255);
+  rect(-width / 2 - 10, -height / 2 - 10, width + 20, height + 20);
+  stroke(101, 67, 33);
+  line(-width, 0, width, 0);
+  stroke(0, 0, 0);
+  fill(0, 0, 0);
+  circle(0, 0, 5);
+}
+
+function updatePosition() {
+  circle(p.info_array[0], p.info_array[1], radius);
 
   f = getForce(p.info_array[0], p.info_array[1]);
 
@@ -85,51 +145,10 @@ function draw() {
   p.add(scaleVector(v, delta_t));
 }
 
-
-/* function to initialize Newton's law and create canvas */
-function initialize(Mass, Gravity, Delta_t, Px, Py, Vx, Vy, Radius, Mu, K, Accel_magnif, Vel_magnif) {
-  mass = Mass;
-  gravity = Gravity;
-  delta_t = Delta_t;
-  p = new Vector(Px, Py);
-  v = new Vector(Vx, Vy);
-  radius = Radius;
-  mu = Mu;
-  k = K;
-  accel_magnif = Accel_magnif;
-  accel_color = [0, 255, 0];
-  vel_magnif = Vel_magnif;
-  vel_color = [255, 0, 0];
-
-  createCanvas(window.innerWidth, window.innerHeight);
-
-  translate(width / 2, height / 2);
-
-  colorMode(RGB);
-  background(255, 255, 255);
-  stroke(101, 67, 33);
-  line(-width, 0, width, 0);
-
-  stroke(0, 0, 0);
-  fill(0, 0, 0);
-
-  circle(0, 0, 5);
-
-  circle(p.info_array[0], p.info_array[1], radius);
-
-  v.setColor(vel_color);
-  v.setMagnif(vel_magnif);
-
-  f = getForce(p.info_array[0], p.info_array[1]);
-
-  a = shrinkVector(f, mass);
-  a.setColor(accel_color);
-  a.setMagnif(accel_magnif);
-  drawVector(p.info_array[0], p.info_array[1], v);
-  drawVector(p.info_array[0], p.info_array[1], a);
-
+function getForce(x, y) {
+  /* Function for force on object at point(x,y) */
+  return simulation_force_functions[selected_simulation](x, y);
 }
-
 
 class Vector {
   /* custom Vector class */
